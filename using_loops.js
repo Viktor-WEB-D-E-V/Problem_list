45b62aa215fe2c827823fe4dc97879957774de17
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-02-28 20:29:10 -1000

    Solved task

--- using_loops.js
+++ using_loops.js
@@ -107,7 +107,6 @@ function reverseArr() {
 
 console.log(changeArr());
 
-
 function buildTriangle(rows) {
   let result = "";
   for (let i = 0; i < rows; i++) {
@@ -126,3 +125,55 @@ function buildTriangle(rows) {
 }
 
 console.log(buildTriangle(5));
+
+function merge(nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+}
+
+// let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3;
+// merge(nums1, m, nums2, n);
+// console.log(nums1);
+
+// nums1 = [1], m = 1, nums2 = [], n = 0;
+// merge(nums1, m, nums2, n);
+// console.log(nums1);
+
+// nums1 = [0], m = 0, nums2 = [1], n = 1;
+// merge(nums1, m, nums2, n);
+// console.log(nums1);
+
+function removeElement(nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+}
+
+let nums1 = [3, 2, 2, 3],
+  val1 = 3;
+let k1 = removeElement(nums1, val1);
+console.log(k1, nums1.slice(0, k1));
+
+let nums2 = [0, 1, 2, 2, 3, 0, 4, 2],
+  val2 = 2;
+let k2 = removeElement(nums2, val2);
+console.log(k2, nums2.slice(0, k2));
