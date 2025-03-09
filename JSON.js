a48b27af53cd5c43376be2b872f198fae9f4b8a8
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-09 00:25:38 -1000

    Added task: Deep Clone an Object (Without JSON). (Using Recursion)

--- JSON.js
+++ JSON.js
@@ -29,14 +29,55 @@
 // 1️. Use the fetch() function to get data from https://jsonplaceholder.typicode.com/users.
 // 2️. Convert the response to JSON.
 // 3️. Display the user in the console.
+// {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log("user list:");
+//       data.forEach((user) => {
+//         console.log(JSON.stringify(user.name));
+//       });
+//     })
+//     .catch((error) => console.error("Error fetching data:", error));
+// }
+
+// Task 3: Deep Clone an Object (Without JSON)
+// Goal: Clone an object without using JSON.stringify(), preserving methods.
+
+// Instructions:
+// 1️. Create an object with a method inside.
+// 2️. Clone it using structured cloning or a utility function.
+// 3️. Verify that changing the clone does not affect the original.
+
 {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("user list:");
-      data.forEach((user) => {
-        console.log(JSON.stringify(user.name));
-      });
-    })
-    .catch((error) => console.error("Error fetching data:", error));
+  function createClone(obj) {
+    const deepClone = {};
+
+    for (let key in obj) {
+      if (typeof obj[key] === "object" && obj[key] !== null) {
+        deepClone[key] = createClone(obj[key]);
+      } else if (typeof obj[key] === "function") {
+        deepClone[key] = obj[key].bind(deepClone);
+      } else {
+        deepClone[key] = obj[key];
+      }
+    }
+    return deepClone;
+  }
+
+  const user = {
+    name: "Viktor",
+    detail: {
+      city: "Lviv",
+      age: 25,
+    },
+    greet() {
+      return `Hello my name is ${this.name}`;
+    },
+  };
+
+  const clonedUser = createClone(user);
+  clonedUser.detail.city = "Winnipeg";
+  console.log(clonedUser);
+  console.log(user);
 }
